bd02e545d9529ea479eb85587edc050cba739b71
Guilherme <guifalcao2017@gmail.com>
2025-02-04 14:12:20 -0300

    Project created

---
+++ src/main/java/jogo_da_forca/App.java
@@ -0,0 +1,13 @@
+package jogo_da_forca;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/jogo_da_forca/AppTest.java
@@ -0,0 +1,20 @@
+package jogo_da_forca;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
